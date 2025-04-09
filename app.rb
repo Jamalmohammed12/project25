d5e0b52a4751bd72aa425086f145db3060272aff
Jamalmohammed12 <Jamal.mohammed@elev.ga.ntig.se>
2025-04-09 23:58:54 +0200

    The last update "innan inlämningen"

--- app.rb
+++ app.rb
@@ -4,22 +4,61 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+
 enable :sessions
-output=[]
+output = []
 
-
+# Database connection
+DB = SQLite3::Database.new "db/Tabeller.db" 
+DB.results_as_hash = true
 
 get('/') do
-    slim(:start)
-  end
+  @ads = DB.execute("SELECT Advertisement.*, Cars.brand, Cars.model FROM Advertisement JOIN Cars ON Advertisement.car_id = Cars.Id")
+  slim :index
+end
 
-  # get('/Advertisements') do
-  #   db = SQLite3::Database.new("db/Tabeller.db")
-  #   db.results_as_hash = true
-  #   @result = db.execute("SELECT * FROM Advertisment")
-  #   slim :index
-  # end
 
- 
+get('/Advertisement') do
+  
+  slim :Advertisement
+end
 
-   
+get('/loggin') do
+  slim :loggin
+end
+
+
+
+
+# Använder '/create_advertisement'för att skapa en nya annonser
+post '/create_advertisement' do
+  # parametrar 
+  brand = params[:brand]
+  model = params[:model]
+  title = params[:title]
+  description = params[:description]
+  price = params[:price]
+  picture = params[:picture]
+
+  # Spara i Cars 
+  DB.execute("INSERT INTO Cars (brand, model) VALUES (?, ?)", [brand, model])
+
+  # Hämta car_id 
+  car_id = DB.last_insert_row_id
+
+  # Spara i Advertisement 
+  DB.execute("INSERT INTO Advertisement (title, description, price, picture, car_id) VALUES (?, ?, ?, ?, ?)", [title, description, price, picture, car_id])
+
+  
+  redirect '/'
+
+end 
+
+post '/delete_advertisement' do
+  ad_id = params[:id].to_i
+  puts "Försöker ta bort annons med id: #{ad_id}"
+
+  DB.execute("DELETE FROM Advertisement WHERE Id = ?", ad_id)
+
+  redirect '/'
+end
